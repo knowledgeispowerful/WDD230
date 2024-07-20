3653399f3204413cadc11c7d53f4ba4fff291da9
knowledgeispowerful <77500783+knowledgeispowerful@users.noreply.github.com>
2024-07-19 19:55:57 -0700

    Update links.js

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,5 @@
-const baseURL = "https://github.com/knowledgeispowerful/wdd230/";
-const linksURL = "https://github.com/knowledgeispowerful/wdd230/data/links.json";
+const baseURL = "https://knowledgeispowerful/wdd230/";
+const linksURL = "https://knowledgeispowerful/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
